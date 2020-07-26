05d62dc40d34bd6fd2c62b95500715054568f640
flpeters <38505891+flpeters@users.noreply.github.com>
2020-07-26 14:07:03 +0200

    add fibonacci example

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+	name    = 'fib',
+	version = '0.1.1',
+	packages = find_packages(),
+	license = 'GPL-2',
+	classifiers = [ 'Developer Status :: 3 - Alpha',
+					'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+					'Natural Language :: English',
+					'Programming Language :: Python :: 3 :: Only',
+					'Programming Language :: Python :: Implementation :: CPython'],
+	ext_modules = [Extension('fib.fib', ['fib/fib.c'])]
+	)
